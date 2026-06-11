8ed40db497a336161c719c3ca4d1a0df0a5a9312
Yaryna Kushniruk <yarynakushniruk@gmail.com>
2026-06-11 13:39:31 +0200

    Added new test for locked_out_user

--- tests/LoginTests.spec.ts
+++ tests/LoginTests.spec.ts
@@ -1,21 +1,38 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../src/pages/LoginPage.ts';
-import { standardUser } from '../src/test-data/users.ts';
+import { standardUser, lockedUser } from '../src/test-data/users.ts';
 
-test('Login Tests', async ({ page }) => {
+test.describe('Login Tests', () => {
+    
+        test('Successful login with valid credentials', async ({ page }) => {
+                const loginPage = new LoginPage(page);
 
-    const loginPage = new LoginPage(page);
+            await test.step('Navigate to login page', async () => {
+                await loginPage.navigate();
+            });
 
-    await test.step('Navigate to login page', async () => {
-        await loginPage.navigate();
-    });
+            await test.step('Login with valid credentials', async () => {
+                await loginPage.login(standardUser.username, standardUser.password);
+            });
 
-    await test.step('Login with valid credentials', async () => {
-        await loginPage.login(standardUser.username, standardUser.password);
-    });
+            await test.step('Verify successful login', async () => {
+                await loginPage.expectLoginSuccess();
+            });
+        })
 
-    await test.step('Verify successful login', async () => {
-        await loginPage.expectLoginSuccess();
-    });
+        test('Failed login with locked out user credentials', async ({ page }) => {
+            const loginPage = new LoginPage(page);
+            await test.step('Navigate to login page', async () => {
+                await loginPage.navigate();
+            });
+
+            await test.step('Login with locked out user credentials', async () => {
+                await loginPage.login(lockedUser.username, lockedUser.password);
+            });
+
+            await test.step('Verify login failure', async () => {
+                await loginPage.expectLoginFailure();
+            });
+        })
 
 });
